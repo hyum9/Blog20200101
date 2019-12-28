5a54e6a870f1e398bcd10715a9ce17906c0b49b7
하연 김 <khy2010@hotmail.com>
2019-12-28 17:39:34 +0900

    commit1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
+//리액트야 Subject 라는 이름의 태그를 나는 만들거야
+//property => props
+
+class Subject extends Component{
+render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <header>
+      <h1>WEB</h1>
+      World Wide Web
+    </header>
+    );
+  }
 }
 
+class TOC extends Component{
+  render(){
+    return(
+        <nav>
+          <ol>
+            <li><a href="1.html">HTML</a></li>
+            <li><a href="2.html">CSS</a></li>
+            <li><a href="3.html">JavaScript</a></li>
+          </ol>
+        </nav>
+    );
+  }
+}
+
+class Content extends Component{
+  render(){
+    return(
+      <article>
+        <h2>Welcome</h2>
+        Hello React.
+      </article>
+
+    );
+  }
+}
+
+class App extends Component {
+  render(){
+    return (
+      <div className ="App">
+        <Subject title="WEB" sub="World wide web"></Subject>
+        <TOC></TOC>
+        <Content></Content>
+      </div>
+    );
+  } 
+}
 export default App;
