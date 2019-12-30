680dd0a7b4c80a499812b43f8f8f3e9509134c7e
하연 김 <khy2010@hotmail.com>
2019-12-30 10:54:35 +0900

    commit20191230T1054

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { HashRouter, Route, Link, Switch } from "react-router-dom";
 
-import Page1 from "./Page1"; //Default Page이다
+import Page1 from "./Page1"; 
 import Page2 from "./Page2"; //Page2 이다
-import Home from "./Home";
+import Home from "./Home"; //Default Page이다
 
 //Route exact path = URL을 지정해주는 것 
 //Component는 해당 URL과 연결되는 페이지를 나타내준다
@@ -11,9 +11,9 @@ import Home from "./Home";
 export default () => (
     <HashRouter>
         <Switch>
-            <Route exact path="/" component={Home}/>
-            <Route exact path="/page1" component={Page1}/>
-            <Route exact path="/page2" component={Page2}/>
+            <Route exact path="/" component = { Home }/>
+            <Route exact path="/page1" component = { Page1 }/>
+            <Route exact path="/page2" component = { Page2 }/>
         </Switch>
     </HashRouter>
 );
