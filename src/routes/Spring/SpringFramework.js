914823d9df475a9615ad02a2f93d8e1d85518e05
하연 김 <khy2010@hotmail.com>
2019-12-30 17:04:03 +0900

    commit20191230T0503

---
+++ src/routes/Spring/SpringFramework.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default () =>(
+    <div>
+        
+
+        통합개발도구환경(IDE): Eclipse<br/>
+        플러그인: Spring-Tool-Suite<br/>
+        ->프로젝트 생성 시 프레임워크로 Spring 사용
+        언어(language): Java<br/>
+        프레임워크(framework): Spring, Maven<br/>
+        -> 프레임워크와 라이브러리
+        웹 서버(web-server): Tomcat<br/><br/><br/><br/>
+
+        <hr/>
+        2019-12-30 ToDoList<br/>
+        1. Blog 화면 바뀌는 거 실행<br/>
+        2. Spring 으로 게시판 만들기 환경 설정 <br/>
+        3. Spring 으로 게시판 만들기 <br/>
+    </div>
+
+);
