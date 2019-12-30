6bf129ec1af74be0db153a5b6fceeffd6e002f76
하연 김 <khy2010@hotmail.com>
2019-12-30 10:17:46 +0900

    commit20191230T10:07

---
+++ src/routes/Page2.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default () =>(
+    <div>
+        Hi
+        <br />
+        <Link to ="/page1">Go to page1</Link>
+    </div>
+);
