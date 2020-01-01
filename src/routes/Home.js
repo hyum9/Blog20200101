182e0994afab01f3ef38af37f43349263b62f823
하연 김 <khy2010@hotmail.com>
2020-01-01 22:55:13 +0900

    commit20200101T1055

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -3,11 +3,6 @@ import { Link } from "react-router-dom";
 
 export default () =>(
     <div>
-<<<<<<< HEAD
       I am at home Now~ See if this working!
-=======
-      This is Home
-      Check if this is working! 
->>>>>>> refs/remotes/origin/master
     </div>
 );
